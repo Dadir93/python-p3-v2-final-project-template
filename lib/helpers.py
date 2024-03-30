0cbabcc2d6d914e1c34959b0275f7d3e87ab232b
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-03-30 23:08:22 +0300

    done

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,61 @@
-# lib/helpers.py
-
-def helper_1():
-    print("Performing useful function#1.")
+from models.department import Department
+from models.employee import Employee
 
 
 def exit_program():
     print("Goodbye!")
     exit()
+
+
+def list_departments():
+    departments = Department.get_all()
+    for department in departments:
+        print(department)
+    
+
+def find_department_by_name():
+    name = input("Enter the department's name: ")
+    department = Department.find_by_name(name)
+    print(department) if department else print (f"Department {name} not found")
+
+
+def find_department_by_id():
+    id_ = input("Enter the department's id: ")
+    department = Department.find_by_id(id_)
+    print(department) if department else print(f"Department {id_} not found")
+
+
+def create_department():
+    name = input("Enter the department's name: ")
+    location = input("Enter the department's location: ")
+    try:
+        department = Department.create(name, location)
+        print(f"Success: {department}")
+    except Exception as exc:
+        print("Error creating department: ", exc)
+
+
+def update_department():
+    id_ = input("Enter the department'd id: ")
+    if department := Department.find_by_id(id_):
+        try:
+            name = input("Enter the department's new name: ")
+            department.name = name 
+            location = input("Enter the department's new location: ")
+            department.location = location
+
+            department.update()
+            print(f"Success: {department}")
+        except Exception as exc:
+            print("Error updating department: ", exc)
+    else:
+        print(f"Department {id_} not found")
+
+
+def delete_department():
+    id_ = input("Enter the department's id: ")
+    if department := Department.find_by_id(id_):
+        department.delete()
+        print(f"Department {id_} deleted")
+    else:
+        print(f"Department {id_} not found")
